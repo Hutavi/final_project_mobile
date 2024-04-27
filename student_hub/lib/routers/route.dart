ea3d3137eb3e4b81c1395a94af87ceaa97958f8d
Huỳnh Tấn Vinh <huynhtanvinhktqn123@gmail.com>
2024-04-27 17:36:58 +0700

    TV:

--- student_hub/lib/routers/route.dart
+++ student_hub/lib/routers/route.dart
@@ -121,25 +121,6 @@ class AppRoute {
           },
         );
 
-      // case AppRouterName.login:
-      //   return PageRouteBuilder(
-      //     pageBuilder: (context, animation, secondaryAnimation) =>
-      //         const LoginScreen(),
-      //     transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      //       const begin = Offset(1.0, 0.0);
-      //       const end = Offset.zero;
-      //       const curve = Curves.ease;
-
-      //       var tween =
-      //           Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-
-      //       return SlideTransition(
-      //         position: animation.drive(tween),
-      //         child: child,
-      //       );
-      //     },
-      //   );
-
       case AppRouterName.login:
         return MaterialPageRoute(builder: (_) => const LoginScreen());
 
